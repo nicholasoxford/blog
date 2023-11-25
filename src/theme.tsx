d470aa631cc372952946a58431f45dd7575da379
Nicholas Oxford <maheledaily@gmail.com>
2023-11-25 11:59:34 -0700

    og image tag?

--- src/theme.tsx
+++ src/theme.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 const MAX_SIDEBAR_LENGTH = 40;
 export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
   const { title, frontMatter, headings, pageMap, route } = pageOpts;
+  console.log({ pageOpts });
   let headers: {
     [key: string]: string;
   }[] = [];
